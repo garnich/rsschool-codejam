e036617370ead71bff553dc9638b6c7c8734c0c7
garnich <garnich@tut.by>
2018-10-28 17:36:56 +0300

    docs: implement function sumOfOther

--- src/sumOfOther.js
+++ src/sumOfOther.js
@@ -1,1 +1,5 @@
-function sumOfOther(arr) {}
+function sumOfOther(arr) {
+    let sum = arr.reduce(function(acc,item){return acc+item});
+    let out = arr.map(currentValue => sum - currentValue);
+    return out;
+}
